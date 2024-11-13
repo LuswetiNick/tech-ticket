bc66831bf52bbc51323c9af9a8f6f23b60b1bd67
LuswetiNick <nicklusweti@protonmail.com>
2024-11-13 15:20:59 +0300

    create 404 html view

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const PORT = process.env.PORT || 8000;
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 
+app.use("/", require("./routes/home"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found!" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
